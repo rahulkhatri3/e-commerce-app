8b417fa5525239a3ddb35b5c746c64e100dacba5
rahulkhatri3 <rahulkhatrirahul100@gmail.com>
2025-05-28 23:27:12 +0100

    added pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import{BrowserRouter as Router, Routes,Route} from "react-router-dom";
 import './App.css';
+import Home from './Home';
+import About from './About';
+import Products from './Products';
+import Contact from './Contact';
+import Singleproduct from './Singleproduct';
+import Carts from './Carts';
+import Error from './Error';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+<Routes>
+  <Route path ="/" element={<Home />}/>
+  <Route path ="/about" element={<About />}/> 
+  <Route path ="/product" element={<Products />}/> 
+  <Route path ="/contact" element={<Contact />}/> 
+    <Route path ="/cart" element={< Carts/>}/> 
+      <Route path ="/error" element={<Error />}/> 
+    <Route path ="/singleproduct/:id" element={<Singleproduct />}/> 
+
+
+</Routes>
+
+    </Router>
+
   );
 }
 
