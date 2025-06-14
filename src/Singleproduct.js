f5ccf043baeb224a16217cbb3763647de5c4f54f
rahulkhatri3 <rahulkhatrirahul100@gmail.com>
2025-06-14 21:53:15 +0530

    single product data provide

--- src/Singleproduct.js
+++ src/Singleproduct.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useProductContext } from './context/Productcontext'
+import { useParams } from 'react-router-dom'
+
+
+const API = "https://api.pujakaitem.com/api/products";
 
 const Singleproduct = () => {
+  const {getSingleProduct, isSingleLoading, singleProduct}=useProductContext()
+  
+  const {id} = useParams();
+
+  const{id:alias, name, company,price,description,category,stock,stars,reviews } =singleProduct
+
+  useEffect(()=>{
+getSingleProduct(`${API}/${id}`)
+
+  },[]);
   return (
-    <div>Singleproduct</div>
+
+    <h1>single page{name}</h1>
+
   )
 }
 
